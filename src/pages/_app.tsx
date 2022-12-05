441cfc97246642b68d99093e0025b957b584cf93
ashizaki <ashizaki.ksk@gmail.com>
2022-12-05 20:49:39 +0900

    フォルダ構成を変更してsrc直下にソースコードを置くように修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css"
+import "styles/globals.css"
 import { Amplify } from "aws-amplify"
 import type { AppProps } from "next/app"
 import "@aws-amplify/ui-react/styles.css"
